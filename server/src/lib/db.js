6e44c76ca48f8a4c88464d77e95f5c6d24bfa260
psycopathic <harshsrivastava2502@gmail.com>
2026-02-26 23:50:24 +0530

    alignment

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -3,7 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = globalThis;
 const prisma = new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") 
+    globalForPrisma.prisma = prisma;
 
 export default prisma;
     
